21ec7f806d64e1d2de7f5593badc9f42f5d184f6
d-sonuga <sonugademilade8703@gmail.com>
2024-09-05 16:54:35 +0100

    removed unnecessary operand iter function

--- src/fastalloc/iter.rs
+++ src/fastalloc/iter.rs
@@ -33,13 +33,6 @@ impl<'a> Operands<'a> {
     pub fn fixed(&self) -> impl Iterator<Item = (usize, Operand)> + 'a {
         self.matches(|op| matches!(op.constraint(), OperandConstraint::FixedReg(_)))
     }
-
-    pub fn non_fixed_use(&self) -> impl Iterator<Item = (usize, Operand)> + 'a {
-        self.matches(|op| {
-            !matches!(op.constraint(), OperandConstraint::FixedReg(_))
-                && op.kind() == OperandKind::Use
-        })
-    }
 }
 
 impl<'a> core::ops::Index<usize> for Operands<'a> {
